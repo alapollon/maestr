148e891a68b31ebd84f78ca4060ab7b69ed3ff77
wyvernghost <65455574+alapollon@users.noreply.github.com>
2025-03-29 06:18:22 -0500

    Create magnum.hpp

---
+++ magnum.hpp
@@ -0,0 +1,80 @@
+//
+//
+#include <stdlib.h>
+//
+//
+#include <stdint.h> 
+//
+//
+#include <stdio.h> 
+//
+//
+#include <unistd.h>
+//
+//
+//
+//
+
+#include <stream.h> 
+#include <ctype.h> 
+#define dword 
+#define word 
+
+// abstract the following 
+//
+
+
+struct stack {
+
+	public: 
+
+
+	private:
+
+}
+
+class DynamicKey {
+	public: 
+		DynamicKey(int size){
+			if size < 0
+	ff = new int[size]
+			this->size = size;
+
+		}
+
+		DynamicKey() {
+			delete[]ff; 
+
+		}
+		int forField[](int index){
+			return forField[index];
+
+		}
+		int getSize() const {
+			return size;
+		}
+	private: :
+		int data;
+		int size;
+}
+struct openkey {
+	char foreword = 
+	
+}
+
+// prepare the following 
+//
+//
+void generation( unsigned ,dword ){
+
+}
+
+// forward declarations
+//
+//
+class superstack {
+
+
+}
+
+#endif 
